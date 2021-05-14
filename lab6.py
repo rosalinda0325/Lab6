4072f2e032bdfd6c9b43f0a1732845329b8940cc
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-14 05:34:19 -0700

    first commit

---
+++ lab6.py
@@ -0,0 +1,14 @@
+
+"""
+This program will generate a random integer between 1 and 20 and based no
+the result of the random number, we check to see if it falls under certain range
+If the number is greater than 15, then it will print "cherries" 
+otherwise if the number is greater > 10 then it will print "orange"
+otherwise if the number is greater > 5 then it will print "plus"
+otherwise if the number is greater > 2 then it will print "melon"
+otherwise if the number is greater > 1 then it will print "bell"
+if the number is not any of the above then it will print "bar"
+
+we start by using a loop 3 times and print the results to the user. Example "plus cherries"
+
+"""
