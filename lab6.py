fbe0eab9d933e71f3f6863b1d34f9172f152b288
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-05-14 05:39:00 -0700

    finished pseudocode

--- lab6.py
+++ lab6.py
@@ -12,3 +12,29 @@ if the number is not any of the above then it will print "bar"
 we start by using a loop 3 times and print the results to the user. Example "plus cherries"
 
 """
+
+"""
+num = generate random number
+
+if num is greater than 15
+then it will print "cherries" 
+
+otherwise if the num is greater > 10
+then it will print "orange"
+
+otherwise if the num is greater > 5 
+then it will print "plus"
+
+otherwise if the num is greater > 2 
+then it will print "melon"
+
+otherwise if the num is greater > 1 
+then it will print "bell"
+
+otherwise
+the result will be "bar"
+
+loop tree times
+print the output (fruit) to the user
+
+"""
